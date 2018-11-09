0a46f68cecf335f0afe4bfc263f1bd37d3bdbd33
kl5mb <kl5mb@mail.missouri.edu>
2018-11-09 11:17:59 -0600

    java

--- menu.js
+++ menu.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
   $('.menu-trigger').click(function(){
-    $('.item'). slideToggle(500);
+    $('#nav .container'). slideToggle(500);
   });
 
 $(window).resize(function(){
   if ($(window).width()>500){
-    $('.item'). removeAttr('style');
+    $('#nav .container'). removeAttr('style');
   }
 });
 });
